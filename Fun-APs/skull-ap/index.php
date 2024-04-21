a0b151f4fb83d0c7ee1a7864c08116d77d848849
ImLilDev <mathdub78@gmail.com>
2024-04-21 21:08:18 +0200

    :package: adding a fun AP

---
+++ Fun-APs/skull-ap/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>MOUHAHAHAHAH</title>
+<link rel="stylesheet" href="style.css">
+<audio autoplay loop>
+  <source src="music.mp3" type="audio/mpeg">
+  Your browser does not support the audio element.
+</audio>
+
+</head>
+<body>
+  <div class="highPage">
+    <div class="skulls">
+      <h1>Internet c'est fini pour aujourd'hui</h1>
+      <table>
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Score</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1st</td>
+          <td>999999</td>
+          <td>MOI</td>
+        </tr>
+        <tr>
+          <td>10th</td>
+          <td>0</td>
+          <td>Toi</td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
+    <center><img src="dance.gif" alt="image de Squelette qui danse"></center>
+  </div>
+</div>
+</div>
+</div>
+    </div>
+  </div>
+  </div>
+  </div>
+</body>
+</html>
